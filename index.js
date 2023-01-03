b76cd0b73137fa62a7b5ef2802f8d62919ddd625
Jesus M <marquez.jesus444@gmail.com>
2023-01-03 17:55:36 -0600

    created array of questions needed for prompt

--- index.js
+++ index.js
@@ -3,7 +3,39 @@ import fs from 'fs';
 import inquirer from 'inquirer';
 import generateMarkdown from './utils/generateMarkdown'
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their Github profiles. List any third-party assets or tutorials were used and include links to the original creators aswell.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project.',
+        choices: ['MIT','ISC' , 'Apache 2.0', 'GNU GPL 3.0', 'None'],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
